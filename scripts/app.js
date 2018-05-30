f9c389b0c38f020478b96e35bee75e00dc217532
julien <julien@Macintosh.local>
2018-05-30 10:41:17 +0200

    ne marche pas

--- scripts/app.js
+++ scripts/app.js
@@ -35,7 +35,7 @@ Instascan.Camera.getCameras().then(function (cameras) {
 });
 
 
-let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: mirror });
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
 
 
 add_to_log('app started');
