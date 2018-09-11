9edebffe025e4690da76ee8d694d96750bc693c6
julien <julien@Macintosh.local>
2018-09-11 15:22:34 +0200

    sw 3

--- scripts/app.js
+++ scripts/app.js
@@ -40,7 +40,7 @@ var date_update = new Date();
 
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../service-worker.js', {scope: 'sw-test'}).then(function(registration) {
+  navigator.serviceWorker.register('service-worker.js', {scope: 'sw-test'}).then(function(registration) {
     // registration worked
     console.log('Registration succeeded.');
     registration.unregister().then(function(boolean) {
